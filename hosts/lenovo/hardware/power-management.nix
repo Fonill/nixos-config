f8f0cb841d2c343bdde3931f10379cdd6a1285ac
Linofff <rowerman2137@gmail.com>
2026-03-20 19:14:58 +0100

    last hope

--- hosts/lenovo/hardware/power-management.nix
+++ hosts/lenovo/hardware/power-management.nix
@@ -9,23 +9,33 @@
   services = {
     system76-scheduler.settings.cfsProfiles.enable = true;
 
+    thermald.enable = true;
+
+    power-profiles-daemon.enable = false;
+
     tlp = {
       enable = true;
       settings = {
-				CPU_BOOST_ON_AC = 1;
-				CPU_BOOST_ON_BAT = 0;
-				CPU_HWP_DYN_BOOST_ON_AC = 1;
-				CPU_HWP_DYN_BOOST_ON_BAT = 1;
+        USB_AUTOSUSPEND = 1;
+        WIFI_PWR_ON_BAT = "on";
+        RUNTIME_PM_ON_BAT = "auto";
+
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+        CPU_HWP_DYN_BOOST_ON_AC = 1;
+        CPU_HWP_DYN_BOOST_ON_BAT = 0;
 
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
+        SOUND_POWER_SAVE_ON_BAT = 1;
+        SOUND_POWER_SAVE_CONTROLLER = "Y";
 
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
-				PLATFORM_PROFILE_ON_AC = "performance";
-				PLATFORM_PROFILE_ON_BAT = "low-power"; # balanced, performance, low-power
+        PLATFORM_PROFILE_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_BAT = "low-power"; # balanced, performance, low-power
 
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
